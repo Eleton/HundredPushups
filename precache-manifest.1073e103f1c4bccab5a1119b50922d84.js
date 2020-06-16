ced68c46d26b1c46cdcf60b85add3561101532b2
Aiman Josefsson <aiman@Aimans-MBP.office.lan>
2020-06-16 20:12:46 +0200

    Updates

---
+++ precache-manifest.1073e103f1c4bccab5a1119b50922d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb2fbccaf389679aeb38ccab07cf44f",
+    "url": "/HundredPushups/index.html"
+  },
+  {
+    "revision": "04a575f5e76015287da6",
+    "url": "/HundredPushups/static/css/main.d6cb4c3a.chunk.css"
+  },
+  {
+    "revision": "b4fb14de5fdac6865bda",
+    "url": "/HundredPushups/static/js/2.57457625.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HundredPushups/static/js/2.57457625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a575f5e76015287da6",
+    "url": "/HundredPushups/static/js/main.32116a5b.chunk.js"
+  },
+  {
+    "revision": "f4374d3922c2cf183c3d",
+    "url": "/HundredPushups/static/js/runtime-main.f215f866.js"
+  }
+]);
